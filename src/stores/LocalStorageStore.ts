90b1d33d6c415bdf629c4311e50e2a7cda207dd1
Roj Serbest <roj@rojser.best>
2021-10-04 18:57:42 +0300

    Use a specific ID for content of LocalStorageStore
    - This is to avoid conflict if there were multiple LocalStorageStores

--- src/stores/LocalStorageStore.ts
+++ src/stores/LocalStorageStore.ts
@@ -2,25 +2,33 @@ import { isBrowser } from "browser-or-node";
 import Store from "./Store";
 
 export default class LocalStorageStore extends Store {
+  private id: string;
+
   constructor() {
     super();
 
     if (!isBrowser) {
       throw new Error("`LocalStorageStore` is only for browsers");
     }
+
+    this.id = String(new Date().getTime());
+  }
+
+  private resolveKey(key: string) {
+    return key + this.id;
   }
 
   set(key: string, value: any) {
-    localStorage.setItem(key, value);
+    localStorage.setItem(this.resolveKey(key), value);
   }
 
   get(key: string) {
-    return localStorage.getItem(key);
+    return localStorage.getItem(this.resolveKey(key));
   }
 
   save() {}
 
   delete(key: string) {
-    localStorage.removeItem(key);
+    localStorage.removeItem(this.resolveKey(key));
   }
 }
