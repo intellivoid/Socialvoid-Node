f70f11c036e7b13d956ea77d70facd62caf5c63a
Roj Serbest <roj@rojser.best>
2021-10-04 12:38:41 +0300

    Finish stores

---
+++ src/stores/index.ts
@@ -0,0 +1,6 @@
+import Store from "./Store";
+import FileStore from "./FileStore";
+import MemoryStore from "./MemoryStore";
+import LocalStorageStore from "./LocalStorageStore";
+
+export { Store, FileStore, MemoryStore, LocalStorageStore };
