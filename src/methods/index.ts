815a9bb34f425dd53c8f7429208542cae4507f8f
Roj Serbest <roj@rojser.best>
2021-10-04 12:41:20 +0300

    Import `Session` methods

--- src/methods/index.ts
+++ src/methods/index.ts
@@ -1,5 +1,6 @@
 import Network from "./Network";
 import Help from "./Help";
 import Cloud from "./Cloud";
+import Session from "./Session";
 
-export { Network, Help, Cloud };
+export { Network, Help, Cloud, Session };
